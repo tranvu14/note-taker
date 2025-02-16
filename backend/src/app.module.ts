d61df8bcbb31a51c12017731d2a3b60b458a55ee
Vu T Nguyen <tranvu.t1.bkdn@gmail.com>
2025-02-16 07:59:50 +0700

    chore: update database url

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -17,11 +17,7 @@ import { Tag } from './notes/entities/tag.entity';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_DATABASE'),
+        url: configService.get('DATABASE_URL'),
         entities: [User, Note, Tag],
         synchronize: configService.get('NODE_ENV') !== 'production',
       }),
